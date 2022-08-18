d5d7f360d2ad7db253c492defd511f17e7ffff78
Vitaly Domnikov <dotcypress@gmail.com>
2022-08-17 18:49:53 -0700

    update hal version

--- src/main.rs
+++ src/main.rs
@@ -11,7 +11,7 @@ extern crate stm32g0xx_hal as hal;
 use defmt_rtt as _;
 
 use core::fmt::Write;
-use hal::gpio::{gpioc::*, *};
+use hal::gpio::{gpiob::*, *};
 use hal::prelude::*;
 use hal::serial::*;
 use hal::stm32;
@@ -26,7 +26,7 @@ mod app {
 
     #[local]
     struct Local {
-        led: PC15<Output<OpenDrain>>,
+        led: PB3<Output<OpenDrain>>,
         timer: Timer<stm32::TIM2>,
         uart: Serial<stm32::USART2, BasicConfig>,
     }
@@ -37,21 +37,22 @@ mod app {
         let mut rcc = ctx.device.RCC.constrain();
 
         let port_a = ctx.device.GPIOA.split(&mut rcc);
-        let port_c = ctx.device.GPIOC.split(&mut rcc);
+        let port_b = ctx.device.GPIOB.split(&mut rcc);
+
+        let mut timer = ctx.device.TIM2.timer(&mut rcc);
+        timer.start(250.millis());
+        timer.listen();
+
+        let led = port_b.pb3.into_open_drain_output();
 
         let uart_cfg = BasicConfig::default().baudrate(115_200.bps());
         let mut uart = ctx
             .device
             .USART2
-            .usart(port_a.pa2, port_a.pa3, uart_cfg, &mut rcc)
+            .usart((port_a.pa2, port_a.pa3), uart_cfg, &mut rcc)
             .expect("Failed to init serial port");
-        uart.write_str("starting...\r\n").ok();
 
-        let mut timer = ctx.device.TIM2.timer(&mut rcc);
-        timer.start(4.hz());
-        timer.listen();
-
-        let led = port_c.pc15.into_open_drain_output();
+        uart.write_str("hello\r\n").ok();
 
         defmt::info!("init completed");
         (Shared {}, Local { timer, led, uart }, init::Monotonics())
@@ -62,6 +63,7 @@ mod app {
         let timer_tick::LocalResources { led, timer, uart } = ctx.local;
 
         led.toggle().ok();
+
         if led.is_set_high().unwrap_or_default() {
             defmt::info!("tick");
             uart.write_str("tick\r\n").ok();
